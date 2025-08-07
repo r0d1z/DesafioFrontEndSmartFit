e09527a8f94eb6f6ada49fc4e8d5f45300f55fa4
Rodrigo Queiroz <rodrigo.guimaraes@icomp.ufam.edu.br>
2025-08-06 22:57:16 -0300

    (feat): add logic to forms and buttons

--- src/app/components/forms/forms.ts
+++ src/app/components/forms/forms.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './forms.html',
   styleUrl: './forms.scss'
 })
-export class Forms {
+export class Forms implements OnInit {
   results: string[] = [];
+  formGroup!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false
+    });
+  }
+
+  onSubmit(): void {
+    console.log("submit");
+    console.log(this.formGroup.value);
+  }
+  onClear(): void {
+    console.log("clear");
+    this.formGroup.reset();
+  }
+
 }
